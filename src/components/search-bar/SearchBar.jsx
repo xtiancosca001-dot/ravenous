abc00e4132e9f785a2efcfb7fc2b408034604c2c
Christian Eric Cosca <ccosca@iripple.com>
2025-05-15 08:54:08 +0800

    style search bar

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -3,18 +3,22 @@ import './search-bar.css';
 
 function SearchBar() {
     return (
-        <div className="search-bar">
-            <div className="sort">
-                <ul>
-                    <li>Best Match</li>
-                    <li>Highest Rated</li>
-                    <li>Most Reviewed</li>
-                </ul>
-            </div>
-            <form className="search-bar" action="#">
-                <input type="search" placeholder="Search Businesses"/> <input type="search" placeholder="Where?"/>
-                <input type="submit" name="Let's Go" />
-            </form>
+        <div className="search-container">
+            <div className="parameters">
+                <div className="sort">
+                    <ul>
+                        <li id='best-match'><a href="#">Best<br/>Match</a></li>
+                        <li id='highest-rated'><a href="#">Highest<br/>Rated</a></li>
+                        <li id='most-reviewed'><a href="#">Most<br/>Reviewed</a></li>
+                    </ul>
+                </div>
+                <form className="search-bar" action="#">
+                    <div className="search-bars">
+                        <input type="search" placeholder="Search Businesses"/> <input type="search" placeholder="Where?"/>
+                    </div>
+                    <button type="submit">Let's Go</button>
+                </form>
+            </div> 
         </div>
     );
 }
