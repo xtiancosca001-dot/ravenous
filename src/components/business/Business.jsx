4f09631bc66fccadfdbbbb0f717e97b1a5135704
Christian Eric Cosca <ccosca@iripple.com>
2025-05-15 23:24:05 +0800

    modify css of business

--- src/components/business/Business.jsx
+++ src/components/business/Business.jsx
@@ -4,19 +4,23 @@ import './business.css';
 function Business({business}) {
     return (
         <div className="business">
-            <img src={business.imageSrc} alt={business.name} />
-            <h2>{business.name}</h2>
-            <div className="address">
-                <p>{business.address}</p>
-                <p>{business.city}</p>
-                <p>
-                    <span className="state">{business.state}</span> <span className="zipcode">{business.zipCode}</span>
-                </p>
+            <div className="image-container">
+                <img src={business.imageSrc} alt={business.name} />
             </div>
-            <div className="review">
-                <p id="category">{business.category}</p>
-                <p id="rating">{business.rating} stars</p>
-                <p>{business.reviewCount} reviews</p>
+            <h2>{business.name}</h2>
+            <div className="business-info">
+                <div className="address">
+                    <p>{business.address}</p>
+                    <p>{business.city}</p>
+                    <p>
+                        <span className="state">{business.state}</span> <span className="zipcode">{business.zipCode}</span>
+                    </p>
+                </div>
+                <div className="review">
+                    <p id="category">{business.category}</p>
+                    <p id="rating">{business.rating} stars</p>
+                    <p>{business.reviewCount} reviews</p>
+                </div>
             </div>
         </div>
     );
